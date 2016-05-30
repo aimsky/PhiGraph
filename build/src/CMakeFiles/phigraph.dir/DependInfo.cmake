c931e67c266ec5e1b45b3b5d1c7cf6ac6928411d
root <wbjustdoit@163.com>
2016-05-30 14:53:10 +0800

    set gitignore

---
+++ build/src/CMakeFiles/phigraph.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wubo/project/github/PhiGraph/src/engine/phigraph_engine.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/engine/phigraph_engine.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/engine/phigraph_init.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/engine/phigraph_init.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/io/graph_io.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/io/graph_io.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/structure/vertex.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/structure/vertex.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/structure/vertexSubset.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/structure/vertexSubset.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/utils/command.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/utils/command.cpp.o"
+  "/home/wubo/project/github/PhiGraph/src/utils/gettime.cpp" "/home/wubo/project/github/PhiGraph/build/src/CMakeFiles/phigraph.dir/utils/gettime.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
